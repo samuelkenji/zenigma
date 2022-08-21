2f7ce2843a6332b6a3e26ceb327a7b627ffea461
samuelkenji <sa.kgs10@gmail.com>
2022-08-21 12:51:30 -0300

    Calculator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const OPERATIONS = {
+  ADD: "ADD",
+  SUBTRACT: "SUBTRACT"
+};
 
 function App() {
+  const [number, updateNumber] = React.useState(0);
+
+  const [state, dispatch] = React.useReducer((state, action) => {
+    /* implement the reducer which should update the state based on the action */
+    switch (action.type) {
+      case "ADD":
+        return parseInt(state) + parseInt(action.value)
+      case "SUBTRACT":
+        return parseInt(state) - parseInt(action.value)
+      default:
+        break;
+    }
+    return state;
+  }, 0);
+
+  /* implement dispatches */
+  const add = () => dispatch({type: OPERATIONS.ADD, value: number});
+  const subtract = () => dispatch({type: OPERATIONS.SUBTRACT, value: number});
+
+  const handleNumberChange = e => updateNumber(Number(e.target.value));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div id="result">{state}</div>
+      <div>
+        <button id="add" onClick={add}>Add</button>
+        <button id="subtract" onClick={subtract}>Subtract</button>
+      </div>
+      <div>
+        <input type="text" value={number} onChange={handleNumberChange} />
+      </div>
     </div>
   );
 }
+
+document.body.innerHTML = "<div id='root'></div>";
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,71 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <App/>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
+/* const Cards = (props) => {
+  const amount = props.amount
+  let cards = []
+  for(let i = 0; i < amount; i++){
+    cards.push(<td className='card' style={{border:'solid black 1px', width:'50px', height:'100px', display:'flex', alignItems:'center', justifyContent:'center', margin:'15px'}}>down</td>)
+  }
+  let table = <table><tbody><tr style={{display:'flex'}}>{cards}</tr></tbody></table>
+
+  return table;
+};
+
+document.body.innerHTML = "<div id='root'> </div>";
+  
+const rootElement = document.getElementById("root");
+ReactDOM.render(<Cards amount={4} />, rootElement);
+
+function turn(e){
+  let state = e.target.innerText;
+  if(state === 'up'){ return }
+
+  let newState;
+  if(state === 'down'){
+    newState = 'up'
+  }else{
+    newState = 'down'
+  }
+
+  let renderedCards = document.getElementsByTagName("td");
+  for(let i = 0; i < renderedCards.length; i++){
+    console.log(renderedCards[i])
+    if(renderedCards[i] !== e.target && renderedCards[i].innerText === newState){
+      return
+    }
+  }
+
+  e.target.innerText = newState;
+
+}
+
+let row = document.getElementById("root").getElementsByTagName("tr")[0];
+
+let renderedCards = document.getElementsByTagName("td");
+console.log(renderedCards)
+for(let i = 0; i < renderedCards.length; i++){
+  console.log(renderedCards[i])
+  renderedCards[i].addEventListener('click', turn)
+}
+
+if(row) {
+  let cell = row.getElementsByTagName("td")[1];
+  if(cell) {
+    cell.click();
+  }
+}
+setTimeout(() => console.log(document.getElementById("root").innerHTML)); */
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
